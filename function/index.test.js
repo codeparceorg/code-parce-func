23c0632680ebf0906520ce7ed644e5981270c359
code parce <codeparcelts@gmail.com>
2026-02-28 16:40:29 -0500

    DEVOPS : UPDATE : Agregar utils y mejorar prueba unitaria

--- function/index.test.js
+++ function/index.test.js
@@ -1,43 +1,18 @@
 const express = require('express');
-const { Firestore } = require('@google-cloud/firestore');
-const { Storage } = require('@google-cloud/storage');
 
 require('dotenv').config();
+
+const { getData } = require('.');
 const app = express();
 app.use(express.json());
 
 
-const storage = new Storage({
-  projectId: process.env.PROJECT_ID
-});
-
-const bucket = storage.bucket(process.env.BUCKET);
-
-const firestore = new Firestore({
-  projectId: process.env.PROJECT_ID,
-  databaseId: process.env.FIRESTORE_DATABASE_ID
-});
-
-app.get('/data', async (req, res) => {
+app.post('/code-parce-func-main', async (req, res) => {
   try {
-    const collection = req.body.collection;
-
-    const snapshot = await firestore.collection(collection).get();
-    let data = null
-
-    if (collection === 'proyects' || collection === 'skills') {
-      console.log("collectio: " + collection)
-      data = await getDataFirestore(snapshot)
-    }
-
-    //console.log(data)
-
-    res.status(200).json(data);
-
+    getData(req, res);
   } catch (error) {
-    console.error(error);
-    const msg = { status: 500, msg: 'Error leyendo Firestore' }
-    res.status(500).json(msg);
+    console.error('Error al obtener datos de Firestore:', error);
+    res.status(500).json({ error: 'Error al obtener datos' });
   }
 });
 
@@ -46,31 +21,3 @@ const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
-
-const getDataFirestore = async (snapshot) => {
-  return await Promise.all(
-    snapshot.docs.map(async (doc) => {
-
-      const docData = doc.data();
-      docData.urls = await Promise.all(
-        docData.images.map(async (img) => {
-          const file = bucket.file("imagenes/" + img);
-
-          const [url] = await file.getSignedUrl({
-            action: 'read',
-            expires: Date.now() + 15 * 60 * 1000, // 15 minutos
-          });
-
-          return url;
-        })
-      );
-
-      return {
-        id: doc.id,
-        ...docData,
-      };
-    })
-  );
-
-}
